3a7232ee51d0ac0764165bcea9a5bed3fb5b6cab
HoonWi <wee0702@gmail.com>
2023-04-14 18:56:10 +0900

    fix notice inner

--- js/main.js
+++ js/main.js
@@ -1,47 +1,50 @@
-const searchEl = document.querySelector('.search');
-const searchInputEL = searchEl.querySelector('input')
+const searchEl = document.querySelector(".search");
+const searchInputEL = searchEl.querySelector("input");
 
-console.log(searchEl)
-console.log(searchInputEL)
+console.log(searchEl);
+console.log(searchInputEL);
 
-searchEl.addEventListener('click', function () {
-  searchInputEL.focus();
-})
+searchEl.addEventListener("click", function () {
+	searchInputEL.focus();
+});
 
-searchInputEL.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEL.setAttribute('placeholder', '통합검색');
-})
+searchInputEL.addEventListener("focus", function () {
+	searchEl.classList.add("focused");
+	searchInputEL.setAttribute("placeholder", "통합검색");
+});
 
-searchInputEL.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEL.setAttribute('placeholder', '');
-})
+searchInputEL.addEventListener("blur", function () {
+	searchEl.classList.remove("focused");
+	searchInputEL.setAttribute("placeholder", "");
+});
 
-const badgeEl = document.querySelector('header .badges');
+const badgeEl = document.querySelector("header .badges");
 
-window.addEventListener('scroll', _.throttle(function() {
-  console.log(window.scrollY);
-  if (window.scrollY > 500){
-    // gsap.to(요소, 지속시간, 옵션);
-    gsap.to(badgeEl, .6, {
-      opacity: 0,
-      display: 'none'
-    });
-  } else {
-    gsap.to(badgeEl, .6, {
-      opacity: 1,
-      display: 'block'
-    });
-  }
-}, 300));
+window.addEventListener(
+	"scroll",
+	_.throttle(function () {
+		console.log(window.scrollY);
+		if (window.scrollY > 500) {
+			// gsap.to(요소, 지속시간, 옵션);
+			gsap.to(badgeEl, 0.6, {
+				opacity: 0,
+				display: "none",
+			});
+		} else {
+			gsap.to(badgeEl, 0.6, {
+				opacity: 1,
+				display: "block",
+			});
+		}
+	}, 300)
+);
 // _.throttle(함수, 시간)
 
 
-const fadeEls = document.querySelectorAll('.visual .fade-in');
-fadeEls.forEach(function(fadeEl, index) {
-  gsap.to(fadeEl, 1, {
-    delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.8
-    opacity: 1,
-  });
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+	gsap.to(fadeEl, 1, {
+		delay: (index + 1) * 0.7, // 0.7, 1.4, 2.1, 2.8
+		opacity: 1,
+	});
 });
