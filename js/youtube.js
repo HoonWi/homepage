dcaedc4a4c9b173076b26a4b5bfb15c4b9226a22
HoonWi <wee0702@gmail.com>
2023-04-27 02:30:17 +0900

    Add youtube iframe

---
+++ js/youtube.js
@@ -0,0 +1,23 @@
+// 매개변수 옵션 https://developers.google.com/youtube/player_parameters?hl=ko
+
+const tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    videoId: 'An6LvWQuj_8',
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      onReady: function (event) {
+        event.target.mute()
+      }
+    }
+  });
+}
