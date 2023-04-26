45ac9743821a83f4cab161dfcc2e53f6339fa34b
HoonWi <wee0702@gmail.com>
2023-04-27 01:37:33 +0900

    Add toggle function

--- js/main.js
+++ js/main.js
@@ -69,7 +69,20 @@ new Swiper('.promotion .swiper-container', {
 		clickable: true,
 	},
 	navigation: {
-		prevEl: '.swiper-prev',
-		nextEl: '.swiper-next',
+		prevEl: '.promotion .swiper-prev',
+		nextEl: '.promotion .swiper-next',
 	},
 });
+
+// Promotion Toggle Btn
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+	isHidePromotion = !isHidePromotion
+	if (isHidePromotion) {
+		promotionEl.classList.add('hide');
+	} else {
+		promotionEl.classList.remove('hide');
+	}
+})
