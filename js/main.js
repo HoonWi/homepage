60b8c253ce65a431dbec2c4fb9b9971de42d6a22
HoonWi <wee0702@gmail.com>
2023-05-15 00:47:01 +0900

    Add Top btn

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEL.addEventListener("blur", function () {
 });
 
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener(
 	"scroll",
@@ -27,15 +28,31 @@ window.addEventListener(
 				opacity: 0,
 				display: "none",
 			});
+			// Top 버튼 숨기기
+			gsap.to(toTopEl, .2, {
+				x: 0
+			});
 		} else {
+			// 배지 숨기기
 			gsap.to(badgeEl, 0.6, {
 				opacity: 1,
 				display: "block",
+			});
+			// Top 버튼 보이기
+			gsap.to(toTopEl, .2, {
+				x: 100
 			});
 		}
 	}, 300)
 );
 // _.throttle(함수, 시간)
+
+
+toTopEl.addEventListener('click', function() {
+	gsap.to(window, .7, {
+		scrollTo: 0
+	});
+})
 
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
